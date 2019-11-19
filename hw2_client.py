41d544c212dfdb4079cef983596076764aa79847
Nimish Agrawal <nimish.agrawal21@gmail.com>
2019-11-18 22:26:23 -0700

    Uploaded clean code

--- hw2_client.py
+++ hw2_client.py
@@ -1,3 +1,9 @@
+'''
+
+Client to test the server
+
+'''
+
 import requests
 
 url = 'http://127.0.0.1:5000/sendJsonData'
--- hw2_server.py
+++ hw2_server.py
@@ -1,17 +1,21 @@
-from flask import Flask, request, jsonify
+'''
+
+CSE535: Assignment-2
+Testing service at http://10.218.107.121:5432/
+
+'''
+
+from flask import Flask, request
 import numpy as np
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from statistics import mode
-import math
-# import requests
 import pickle
 import json
 
 app = Flask(__name__)
 actions = {"buy": 1, "communicate":2, "fun": 3, "hope": 4, "mother":5,"really":6}
-
 
 
 def convert_to_csv(json_data):
@@ -24,11 +28,7 @@ def convert_to_csv(json_data):
                'leftHip_y', 'rightHip_score', 'rightHip_x', 'rightHip_y', 'leftKnee_score', 'leftKnee_x', 'leftKnee_y',
                'rightKnee_score', 'rightKnee_x', 'rightKnee_y', 'leftAnkle_score', 'leftAnkle_x', 'leftAnkle_y',
                'rightAnkle_score', 'rightAnkle_x', 'rightAnkle_y']
-    # data = json.loads(open(path_to_video, 'r').read())
-    # print(json_data)
     data = json_data
-    # print type(data)
-    # print data
     csv_data = np.zeros((len(data), len(columns)))
     for i in range(csv_data.shape[0]):
         one = []
@@ -39,10 +39,10 @@ def convert_to_csv(json_data):
             one.append(obj['position']['y'])
         csv_data[i] = np.array(one)
     # path_to_video = path_to_video[:-5]
-    return pd.DataFrame(csv_data, columns=columns)# .to_csv(path_to_video + 'key_points.csv', index_label='Frames#'
+    return pd.DataFrame(csv_data, columns=columns)
 
 
-
+# Only required for training or it the request is a GET request
 def load_data(df, y, actions):
     # Please change the path of the file accordingly
     data_path = "CSV/"
@@ -55,12 +55,11 @@ def load_data(df, y, actions):
                 y_temp = pd.DataFrame([actions[action]]*df_temp.shape[0])
                 y = pd.concat([y, y_temp])
     df = df.drop(df.columns[[0, 1]], axis=1) # Removing the scores from the feature extraction
-    # if (df.isnull().values.any()) or (math.isnan(df)):
-    #     df.fillna(df.mean())
     train_x, test_x, train_y, test_y = train_test_split(df, y, test_size = 0.30, shuffle = True)
     return train_x, test_x, train_y, test_y
 
 
+# prediction using Logistic Regression
 def logistic_regression(train_x, test_x, train_y, test_y):
     loaded_model = pickle.load(open("logistic_regression_model.sav", 'rb'))
     lr_prediction = loaded_model.predict(test_x)
@@ -72,7 +71,8 @@ def logistic_regression(train_x, test_x, train_y, test_y):
     return label
 
 
-def Decision_tree(train_x, test_x, train_y, test_y):
+# prediction using a Decision Tree
+def decision_tree(train_x, test_x, train_y, test_y):
     loaded_model = pickle.load(open("decision_tree_model.sav", 'rb'))
     decision_prediction = loaded_model.predict(test_x)
     # print("Accuracy for Decision Tree Classifier = ", loaded_model.score(test_x, test_y)*100)
@@ -83,7 +83,8 @@ def Decision_tree(train_x, test_x, train_y, test_y):
     return label
 
 
-def Neural_Network(train_x, test_x, train_y, test_y):
+# prediction using NN
+def neural_network(train_x, test_x, train_y, test_y):
     loaded_model = pickle.load(open("neural_network_model.sav", 'rb'))
     nn_prediction = loaded_model.predict(test_x)
     # print("Accuracy for Neural Network Classifier = ", loaded_model.score(test_x, test_y)*100)
@@ -94,9 +95,9 @@ def Neural_Network(train_x, test_x, train_y, test_y):
     return label
 
 
-# SVM Will take approximately 10-15 minutes to give result. Please wait.
-def SVM(train_x, test_x, train_y, test_y):
-    print("inside svm")
+# prediction using SVM
+# SVM Will take approximately 10-15 minutes, if training
+def svm(train_x, test_x, train_y, test_y):
     loaded_model = pickle.load(open("svm_classifier_model.sav", 'rb'))
     svm_prediction = loaded_model.predict(test_x)
     # print("Accuracy for SVM = ", loaded_model.score(test_x, test_y)*100)
@@ -107,18 +108,17 @@ def SVM(train_x, test_x, train_y, test_y):
     return label
 
 
+# GET request
 @app.route('/')
-def main():
-    print("got a get request")
+def main_get():
+    print("Received a GET request")
     df = pd.DataFrame()
     y = pd.DataFrame()
-    
     train_x, test_x, train_y, test_y = load_data(df, y, actions)
-    
     label_lr = logistic_regression(train_x, test_x, train_y, test_y)
-    label_dt = Decision_tree(train_x, test_x, train_y, test_y)
-    label_nn = Neural_Network(train_x, test_x, train_y, test_y)
-    label_svm = SVM(train_x, test_x, train_y, test_y)
+    label_dt = decision_tree(train_x, test_x, train_y, test_y)
+    label_nn = neural_network(train_x, test_x, train_y, test_y)
+    label_svm = svm(train_x, test_x, train_y, test_y)
     
     result = {"1" : label_lr, 
               "2" : label_dt, 
@@ -129,58 +129,32 @@ def main():
     return str(result)
 
 
+# POST request
 @app.route('/sendJsonData', methods=['GET', 'POST'])
-def mainJson():    
+def main_post():
+    print("Received a POST request")
     content = request.get_json(force=True)
-    # content = request.data
-    # print(content)
     test_x = None
-    if(content != None):
+    if (content != None):
         test_x = convert_to_csv(content)
         print(test_x)
         test_x = test_x.drop(test_x.columns[[0]], axis=1)  # Removing the scores from the feature extraction
-        # if (test_x.isnull().values.any()) or (math.isnan(test_x)):
-        #     test_x.fillna(test_x.mean(), inplace=True)
+        print(test_x.shape)
+        # replace missing values with mean
+        test_x.fillna(test_x.mean(), inplace=True)
     print("\n\n\n\n")
-    # print(test_x)
-
-    df = pd.DataFrame()
-    y = pd.DataFrame()
-
-    # train_x, test_xx, train_y, test_y = load_data(df, y, actions)
     train_x = None
     train_y = None
     test_y = None
     label_lr = str(logistic_regression(train_x, test_x, train_y, test_y))
-    label_dt = str(Decision_tree(train_x, test_x, train_y, test_y))
-    label_nn = str(Neural_Network(train_x, test_x, train_y, test_y))
-    label_svm = str(SVM(train_x, test_x, train_y, test_y))
+    label_dt = str(decision_tree(train_x, test_x, train_y, test_y))
+    label_nn = str(neural_network(train_x, test_x, train_y, test_y))
+    label_svm = str(svm(train_x, test_x, train_y, test_y))
     
     result = {"1" : label_lr,
               "2" : label_dt, 
               "3" : label_nn,
-              "4" : label_svm} 
-    # print(result)
-    # result = str(result).replace("'", "\"")
+              "4" : label_svm}
     result = json.dumps(result)
     print(result)
     return str(result)
-
-
-
-'''
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-'''
-
-# @app.route('/', methods = ['POST'])
-# def handle_request():
-#     imagefile = flask.request.files['video']
-#     print("Received Video :" + imagefile.filename)
-#     # imagefile.save(filename)
-#     return "Video uploaded Successfully!"
-# @app.route('/', methods = ['GET'])
-# def handle_requests():
-#     return "Flask Server & Android are Working Well!"
-# app.run(host = "0.0.0.0", port =5000,debug = True)
